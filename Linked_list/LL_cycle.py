1eb22144735ec54e72d3ecdec26b2b6a97260aaf
anjali yadav <anjali2360ydv@gmail.com>
2025-12-21 16:03:58 +0530

    cycle detection in linked list

---
+++ Linked_list/LL_cycle.py
@@ -0,0 +1,48 @@
+"""
+LEETCODE PROBLEM 181
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+"""
+# FLOYD'S CYCLE DETECTION(two pointers)
+
+def find_cycle(self,head):
+    slow=fast=head
+
+    while fast and fast.next:
+        slow=slow.next
+        fast=fast.next.next
+
+        if fast==slow:
+            return True
+        
+    return False
+
+# USING SET
+
+def has_cycle(self,head):
+    seen=set()
+
+    while head:
+        
+        if head in seen:
+            return True
+        
+        seen.add(head)
+        head=head.next
+    
+    return False
